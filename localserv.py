ef87e713c98f45d46f9af043e403d48694fa88ef
Tim <Timinator01@gmail.com>
2018-09-11 14:55:35 -0400

    added localserv to preview sites - tim

---
+++ localserv.py
@@ -0,0 +1,30 @@
+# webtest.py
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import os
+import webbrowser
+
+
+class Server(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        print("do_get")
+        if self.path == '/':
+            self.path = '/index.html'
+        try:
+            page = open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            page = "Page not Found"
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(page, 'utf-8'))
+
+
+def preview_site(path):
+    os.chdir(path)
+    httpd = HTTPServer(('localhost', 8080), Server)
+    httpd.serve_forever()
+    webbrowser.open('localhost:8080', new=1, autoraise=True)
+
+
+# previewSite("/home/beanz/cs370/folder/")
