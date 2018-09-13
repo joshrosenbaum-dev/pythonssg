4606af52457bf98375578b3b655df3689438d148
Josh Rosenbaum <34691747+blurryjoshy@users.noreply.github.com>
2018-09-13 15:09:36 -0400

    Add files via upload

---
+++ build.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+import markdown
+import localserv
+
+srcpath = os.getcwd() + "/src/"
+wwwpath = os.getcwd() + "/www"
+bkppath = os.getcwd() + "/www_bkp"
+
+def md2html():
+    for filename in os.listdir(srcpath):
+        if filename.endswith(".md"):
+            input_file = open(srcpath + filename)
+            text = input_file.read()
+            html = markdown.markdown(text)
+            os.mkdir(wwwpath + "/" + filename.replace(".md", ""))
+            output_file = open(wwwpath + "/" + filename.replace(".md", "") + "/" + "index.html", "w")
+            output_file.write(html)
+            print("Converted MD (.md) files are now in web directory!")
+            continue
+        else:
+            continue
+
+if not os.path.exists(srcpath):
+    os.mkdir(srcpath)
+    print("There was no source directory to work with. Please place Markdown (.md) files into the folder named '/src'.")
+    exit()
+
+if os.path.exists(bkppath):
+    print("Deleting past backups...")
+    shutil.rmtree(bkppath)
+    print("Done!")
+
+if os.path.exists(wwwpath):
+    print("Creating backup...")
+    shutil.copytree(wwwpath, bkppath)
+    print("Backup created!")
+    print("Reinitializing web directory...")
+    shutil.rmtree(wwwpath)
+    os.mkdir(wwwpath)
+    print("Web directory created!")
+    md2html()
+else:
+    print("Initializing web directory...")
+    os.mkdir(wwwpath)
+    print("Web directory created!")
+    md2html()
+
+print("[SUCCESS] Launching web server on localhost:8080")
+localserv.preview_site(wwwpath + "/home")
