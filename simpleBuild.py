525db2677e38e4aa930a6c4099b28686ae7d9b73
blurryjoshy <j.rosenbaum1102@gmail.com>
2018-09-27 15:19:07 -0400

    Modified concept/header

--- simpleBuild.py
+++ simpleBuild.py
@@ -1,9 +1,9 @@
 # Simple Website Creation
-# Author:   Josh Rosenbaum, 09/27/2018
-#
-# Concept:  To generate the website package for the user to upload freely.
-#           Module invoked by command line with argument:
-#           `-b or --build projectNameHere`
+# Author: Josh Rosenbaum, 09/27/2018
+
+# Concept: To generate the website package for the user to upload freely.
+#          Module invoked by command line with argument:
+#          `-b or --build projectNameHere`
 
 
 import os
