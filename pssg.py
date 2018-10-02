817dbf7adc333034e7c2e9095edb5746185576e5
Tim Rogers <Timinator01@gmail.com>
2018-10-02 15:38:50 -0400

    removed need for -f flag in CLI per josh's request

--- pssg.py
+++ pssg.py
@@ -10,23 +10,23 @@ def main():
 
     # Arguments go here cmd group is mutually exclusive
     cmd = parser.add_mutually_exclusive_group()
-    cmd.add_argument('-n', '--new', help="new project", action='store_true')
-    cmd.add_argument('-b', '--build', help="build project", action='store_true')
+    cmd.add_argument('-n', '--new', help="new project", action='store', dest='newfile')
+    cmd.add_argument('-b', '--build', help="build project", action='store', dest='buildfile')
     parser.add_argument('-s', '--serve', help="start preview server", action='store_true')
-    parser.add_argument('-f', '-filename', help="name of project folder", action='store', dest='filename')
+    # parser.add_argument('-f', '-filename', help="name of project folder", action='store', dest='filename')
 
     args = parser.parse_args()
 
     # basic calls based on arguments run 'python PSSG.py -h' for usage
-    if args.new:
-        if not os.path.exists(args.filename):
-            print("Making new project: " + args.filename)
-            os.mkdir(args.filename)
+    if args.newfile:
+        if not os.path.exists(args.newfile):
+            print("Making new project: " + args.newfile)
+            os.mkdir(args.newfile)
         else:
             print("This Directory already exists")
 
-    elif args.build:
-        projName = args.filename
+    elif args.buildfile:
+        projName = args.buildfile
         projPath = os.path.join(os.getcwd(), "Projects", projName)
         simpleBuild.build(projPath)
         if args.serve:
