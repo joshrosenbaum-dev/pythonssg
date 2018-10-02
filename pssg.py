1d4087952c72ec2ea1b9dca70dd9c77928c0531d
Tim Rogers <Timinator01@gmail.com>
2018-10-02 15:21:07 -0400

    Added "New project" support and Print usage if no arguments are given

--- pssg.py
+++ pssg.py
@@ -1,32 +1,39 @@
-#PSSG.py - CLI driver for Pumpkin Spice Static Site Generator
+# PSSG.py - CLI driver for Pumpkin Spice Static Site Generator
 
 import argparse
 import os
 import simpleBuild
 
+
 def main():
     parser = argparse.ArgumentParser(description="PSSG CLI")
 
-    #Arguments go here cmd group is mutually exclusive
+    # Arguments go here cmd group is mutually exclusive
     cmd = parser.add_mutually_exclusive_group()
-    cmd.add_argument('-n','--new',help="new project",action='store_true')
-    cmd.add_argument('-b','--build',help="build project", action='store_true')
-    parser.add_argument('-s','--serve',help="start preview server",action='store_true')
-    parser.add_argument('-f','-filename',help="name of project folder", action='store', dest='filename')
+    cmd.add_argument('-n', '--new', help="new project", action='store_true')
+    cmd.add_argument('-b', '--build', help="build project", action='store_true')
+    parser.add_argument('-s', '--serve', help="start preview server", action='store_true')
+    parser.add_argument('-f', '-filename', help="name of project folder", action='store', dest='filename')
 
     args = parser.parse_args()
 
-    #basic calls based on arguments run 'python PSSG.py -h' for usage
+    # basic calls based on arguments run 'python PSSG.py -h' for usage
     if args.new:
-        print("Making new project: "+args.filename)
-        #TODO: call new project function
+        if not os.path.exists(args.filename):
+            print("Making new project: " + args.filename)
+            os.mkdir(args.filename)
+        else:
+            print("This Directory already exists")
+
     elif args.build:
         projName = args.filename
         projPath = os.path.join(os.getcwd(), "Projects", projName)
         simpleBuild.build(projPath)
         if args.serve:
             print("starting preview server")
-            #TODO: call server function
+            # TODO: call server function
+    else:
+         parser.print_help()
 
 
 if __name__ == "__main__":
