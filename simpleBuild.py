2e3e59c590a00ee49daabba5c8d839383d08b31d
blurryjoshy <j.rosenbaum1102@gmail.com>
2018-10-02 15:37:10 -0400

    Removed redundancies in files (replicated CLI and variables)

--- simpleBuild.py
+++ simpleBuild.py
@@ -20,23 +20,16 @@ def build(projPath):
     else:
         print("[OK] Found project directory")
     if not os.path.exists(pkgPath):
+
         os.makedirs(pkgPath)
         print("[OK] Initialized project website package")  # pkgPath created on first run
-        #TODO: md2html goes here
-        md2html.mdConvert(projPath, newPath)
+        md2html.mdConvert(projPath, pkgPath)
         print("[OK] Build finished!")
     else:
         print("[OK] Package already exists. Regenerating project website package")
         os.makedirs(newPath)  # newPath created because pkgPath already exists
-        tmpFile = open(os.path.join(newPath, "file.html"), "w")
-        tmpFile.write("<h1>Hello, Starbucks.</h1><p>Temporary</p>")
-        tmpFile.close()  # file is place in newPath to show a change in file structure vs. empty test case
-        # TODO: md2html goes here, above lines removed
         md2html.mdConvert(projPath, newPath)
         os.renames(pkgPath, tmpPath)  # _www is renamed to _tmp
         os.renames(newPath, pkgPath)  # _new is renamed to _www
         shutil.rmtree(tmpPath)  # delete _tmp
         print("[OK] Build finished!")
-
-# projPath = os.path.join(os.getcwd(), "Projects", "testProject") # Replicating CLI
-# build(projPath)
