6cd305b6257ade6030004ec51a0e9c2bed16ab30
blurryjoshy <j.rosenbaum1102@gmail.com>
2018-10-02 15:04:17 -0400

    Added TODO for clarification

--- simpleBuild.py
+++ simpleBuild.py
@@ -21,6 +21,7 @@ def build(projPath):
     if not os.path.exists(pkgPath):
         os.makedirs(pkgPath)
         print("[OK] Initialized project website package")  # pkgPath created on first run
+        #TODO: md2html goes here
         print("[OK] Build finished!")
     else:
         print("[OK] Package already exists. Regenerating project website package")
@@ -28,6 +29,7 @@ def build(projPath):
         tmpFile = open(os.path.join(newPath, "file.html"), "w")
         tmpFile.write("<h1>Hello, Starbucks.</h1><p>Temporary</p>")
         tmpFile.close()  # file is place in newPath to show a change in file structure vs. empty test case
+        # TODO: md2html goes here, above lines removed
         os.renames(pkgPath, tmpPath)  # _www is renamed to _tmp
         os.renames(newPath, pkgPath)  # _new is renamed to _www
         shutil.rmtree(tmpPath)  # delete _tmp
